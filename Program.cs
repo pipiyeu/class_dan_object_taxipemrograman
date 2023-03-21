a2e7b0a3bf9a7b2cfa975cba5e1e7cef5334b4b9
pipiyeu <luthfiyyahNN3073@gmail.com>
2023-03-21 14:01:09 +0700

    membuat class dan object

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuattaxi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.drivername = "Andrea";
+            taxi.onduty = false;
+            taxi.numpassenger = 10;
+
+            taxi.taxiinfo();
+            taxi.pickuppassenger();
+            taxi.dropoffpassenger();
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuattaxi
+{
+    public class taxi
+    {
+        public string drivername { get; set; }
+        public Boolean onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver name : {0}", drivername);
+
+            if (onduty == true)
+            {
+                Console.WriteLine("On duty: Yes", onduty);
+            } else if (onduty == false)
+            {
+                Console.WriteLine("On duty: No", onduty);
+            }
+
+            Console.WriteLine("Number Of Passenger: {0}", numpassenger);
+        }
+
+        public void pickuppassenger()
+        { 
+
+        if (onduty == true)
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", drivername);
+        }else if (onduty == false)
+            {
+                Console.WriteLine("\n{0} sedang tidak menjemput penumpang", drivername);
+            }
+        }
+
+        public void dropoffpassenger()
+        {
+            if (onduty == true)
+            {
+                Console.WriteLine("{0} sedang mengantar penumpang", drivername);
+            } else if (onduty == false)
+                {
+                    Console.WriteLine("{0} sedang tidak mengantar penumpang", drivername);
+                }
+            
+        }
+
+    }
+}
